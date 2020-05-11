5110f2be83ee3397f37e54e9410219cadc7e3b45
iamdaniyalz.pro@gmail.com <Daniyal>
2020-05-11 07:56:51 +0500

    invonvde edu=it

--- cypress/integration/page objects/editor_objects.js
+++ cypress/integration/page objects/editor_objects.js
@@ -1,34 +1,21 @@
 /// <reference types="Cypress" />
 
-const textA = "div .blockbuilder-content-tool:nth-child(6)"
-const button = "li.nav-item:nth-child(2)"
-
-
-const username = ".dropdown-toggle > div > span:nth-child(1)"
-const inboxSelector = ".dropdown-menu  select[ng-model='identity.selectedInbox']"
-
-//Navigation bar Objects
-const hamburgerButton = "a[ng-click='minimalize()']"
-const navBar = "#side-menu"
-const logo = ".nav-header a[href='#/welcome']"
-const welcome = "li[ui-sref-active='active'] > a[href='#/welcome']"
-const upload = ".sidebar-collapse a[href='#/upload']"
-const inbox = ".sidebar-collapse a[href='#/inbox/']"
-const review = ".sidebar-collapse a[href='#/view/']"
-const neverTwice = "li[ng-if='showNeverTwiceMenu()']"
-const list = ".sidebar-collapse a[href='#/never_twice_list']"
-const studio = '#ta-studio'
-const backend = ".sidebar-collapse a[href='#/studio_backends']"
-const documentation = "li[ng-if='showDocumentationMenu()']"
-const userGuide = ".sidebar-collapse a[href='#/doc_user_guide']"
-const settings = "li[ng-if='showSettingsMenu()']"
-const billing = ".sidebar-collapse a[href='#/ov_billing']"
-const apiKeys = ".sidebar-collapse a[href='#/ov_api_keys']"
+const textA = "div.blockbuilder-content-tool:nth-child(6)"
+const body = "li.nav-item:nth-child(3)"
+const bgColor = "div[class='sc-uJMKN eWrJFr']:nth-child(2)"
 
 class Tools {
 
     textA() {
         return textA
+    }
+
+    body() {
+        return body
+    }
+
+    bgColor() {
+        return bgColor
     }
 }
 
--- cypress/integration/webpages/editor.js
+++ cypress/integration/webpages/editor.js
@@ -1,60 +1,92 @@
 ///<reference types="Cypress" />
 
 require('cypress-iframe');
-require('@4tw/cypress-drag-drop')
 import { Tools } from "../page objects/editor_objects";
 
 const tools = new Tools()
 
+const getIframeDocument = () => {
+  return cy
+  .get('#editor-1 > iframe')
+  // Cypress yields jQuery element, which has the real
+  // DOM element under property "0".
+  // From the real DOM iframe element we can get
+  // the "document" element, it is stored in "contentDocument" property
+  // Cypress "its" command can access deep properties using dot notation
+  // https://on.cypress.io/its
+  .its('0.contentDocument').should('exist')
+}
 
+const getIframeBody = () => {
+  // get the document
+  return getIframeDocument()
+  // automatically retries until body is loaded
+  .its('body').should('not.be.undefined')
+  // wraps "body" DOM element to allow
+  // chaining more Cypress commands, like ".find(...)"
+  .then(cy.wrap)
+}
 
 function movePiece (selector, x, y) {
-  //cy.get(selector)
-  // cy.iframe().find(selector)
-  // .trigger('mousedown', { which: 1 }, { force: true })
-  // .trigger('mousemove', { clientX: x, clientY: y }, { force: true })
-  // .trigger('mouseup', { force: true })
-  cy.iframe().find(selector)
-      .trigger('click')
-    //   .trigger('mousedown', { button: 0 , force: true , view: window})
-    //   .trigger('mousemove', {
-    //   clientX: x,
-    //     clientY: y,
-    //   screenX: x,
-    //     screenY: y,
-    //   pageX: x,
-    //     pageY: y
-    //   }, { force: true, view: window })
-    // .trigger('mouseup', { force: true })
+  getIframeBody().find(selector)
+      .trigger('mousedown', { which: 1 , force: true , view: window})
+      .trigger('mousemove', {
+      clientX: x,
+        clientY: y,
+      screenX: x,
+        screenY: y,
+      pageX: x,
+        pageY: y
+      }, { force: true, view: window })
+    .trigger('mouseup', { force: true })
 
 }
 
 describe('Navigate to the email editor', () => {
   it('Editor should open', () => {
     cy.visit('') //Fetches the URL from cypress.json      
-      //cy.switchToIframe('iframe',tools.textA).as('tool_textA')
-      // cy.get('iframe')
-      //   .iframe('li.nav-item:nth-child(2)')
-      //   .should('exist')
-      cy.frameLoaded("iframe:nth-child(1)")
+    //cy.frameLoaded("iframe:nth-child(1)")
+ 
+    //Tried the following approaches too
+        //cy.switchToIframe('iframe',tools.textA).as('tool_textA')
 
+        // cy.get('iframe')
+        //   .iframe('li.nav-item:nth-child(2)')
+        //   .should('exist')
   })  
 })
 
 
 describe('Drag and Drop A', () => {
   it('Drag A', () => {
-    // cy.iframe().find('li.nav-item:nth-child(2)').should('be.visible').click()
-    // cy.reload() 
-    //cy.iframe().find('#u_column_1 > div > div > div.blockbuilder-layer.blockbuilder-layer-selectable > div.blockbuilder-layer-selector.blockbuilder-layer-selector-first').trigger('mousedown')
-    //cy.iframe().find('div.blockbuilder-content-tool:nth-child(6)').move({x:300, y:100})
-    movePiece("div.blockbuilder-content-tool:nth-child(6)", 250, 100)
-// cy.iframe().find("div.blockbuilder-content-tool:nth-child(6)").dragTo("#u_column_1 > div > div > div.blockbuilder-layer.blockbuilder-layer-selectable > div.blockbuilder-layer-selector.blockbuilder-layer-selector-first");
-// cy.iframe().find('#u_column_1 > div > div > div.blockbuilder-layer.blockbuilder-layer-selectable > div.blockbuilder-layer-selector.blockbuilder-layer-selector-first').click()
-// cy.iframe().find('div.blockbuilder-content-tool:nth-child(6)')
-//   .trigger('mousedown', { which: 1, pageX: 600, pageY: 100 }, { force: true })
-//   .trigger('mousemove', { which: 1, pageX: 600, pageY: 600 }, { force: true })
-//   .trigger('mouseup', { force: true })
+
+    movePiece(tools.textA(), 300, 150)
+
+    //Tried the following solutions for Drag A but got same result as the above movePiece function
+      //  cy.iframe().find('div.blockbuilder-content-tool:nth-child(6)').move({x:300, y:100})
+      
+      //  cy.iframe().find("div.blockbuilder-content-tool:nth-child(6)").dragTo("#u_column_1 > div > div > div.blockbuilder-layer.blockbuilder-layer-selectable > div.blockbuilder-layer-selector.blockbuilder-layer-selector-first");
+      
+      //  getIframeBody().find("div.blockbuilder-content-tool:nth-child(6)").dragTo("#u_column_1 > div > div > div.blockbuilder-layer.blockbuilder-layer-selectable > div.blockbuilder-layer-selector.blockbuilder-layer-selector-first");
+
+      //  cy.iframe().find('div.blockbuilder-content-tool:nth-child(6)')
+        //   .trigger('mousedown', { which: 1, pageX: 600, pageY: 100 }, { force: true })
+        //   .trigger('mousemove', { which: 1, pageX: 600, pageY: 600 }, { force: true })
+        //   .trigger('mouseup', { force: true })
 
   })  
+})
+
+describe('Change Colour', () => {
+  it('Go to body tab', () => {
+    getIframeBody().find(tools.body()).click()
+  })
+
+  it('Change color', () => {
+      // select the new color value in the <input type="color">
+      // element and trigger "change" event
+      getIframeBody().find(tools.bgColor())
+          .invoke('val', '#000000')
+          .trigger('change')
+  })
 })
